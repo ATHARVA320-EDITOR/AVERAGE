da67c8f743d79c9b4b91383a9f51a7e02754853a
ATHARVA320-EDITOR <ddnatharva@gmail.com>
2024-10-03 16:19:46 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+tree1 = 98
+tree2 = 30
+tree3 = 94
+tree4 = 87
+tree5 = 11
+sum = tree1+tree2+tree3+tree4+tree5
+print("The sum of all the trees is: ", sum)
+average = sum/5
+print("The average of all the trees is:", average)
